e8cccf82d2daac5f7158799eade6cbc2f7faf508
Justin Pezzack <justin.pezzack@gmail.com>
2016-05-21 14:30:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port : "8000",
-  bot_name : "gpa.bot",
-  api_key : "052020a2-1222-44d6-adf1-c7a0c1cef43e",
-  uw_api_token: "f3bf44dd39b2b55a98f0ea49390b157c",
-  base_url : 'https://9ed46653.ngrok.io'
+  port : "<your_port>",
+  bot_name : "<your_bot_name>",
+  api_key : "<your_api_key>",
+  uw_api_token: "<your_uw_api_key>",
+  base_url : 'https://xxxxxxxx.ngrok.io' //for testing purposes use ngrok
 };
