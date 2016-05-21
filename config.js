cfcad0b4ee5620f9f320e84e5293ac6a052c6a2d
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-21 15:18:13 -0400

    api

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port : "8000",
-  bot_name : "gpa.bot",
-  api_key : "052020a2-1222-44d6-adf1-c7a0c1cef43e",
-  uw_api_token: "f3bf44dd39b2b55a98f0ea49390b157c",
-  base_url : 'https://c91b744d.ngrok.io'
+  port         : "<your_port>",
+  bot_name     : "<your_bot_name>",
+  api_key      : "<your_api_key>",
+  uw_api_token : "<your_uw_api_key>",
+  base_url     : 'https://xxxx.ngrok.io' //for testing purposes use ngrok
 };
