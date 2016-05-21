1bb71ef178326aaaa991bf1fc06ff97a6adaecfc
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-21 00:36:18 -0400

    v1 cafeteria bot

--- bot.js
+++ bot.js
@@ -1,39 +1,46 @@
+//Imports
 'use strict';
-
 let util = require('util');
 let http = require('http');
 let Bot  = require('@kikinteractive/kik');
 let config = require('./config');
-
-// Configure the bot API endpoint, details for your bot
-console.log(config);
+let apiToken = 'f3bf44dd39b2b55a98f0ea49390b157c';
+let uwapi = require('uwapi')(apiToken);
+uwapi.foodservicesMenu().then(function(data) {
+  console.log(data);
+});
+/////////////////API ENDPOINT CONFIG////////////////////////
 let bot = new Bot({
     username: config.bot_name,
     apiKey: config.api_key,
     baseUrl: config.base_url
 });
-//DEV
-bot.send('Bot Active', 'justinpezzack');
-
 bot.updateBotConfiguration();
-//Events
+///////////////////////////DEV///////////////////////////////
+bot.send('Bot Active', 'justinpezzack');
+///////////////////////EVENT HANDLERS////////////////////////
 //Fires when a user talks to the bot for the very first time
 bot.onStartChattingMessage((message) => {
     console.log('New user');
     bot.getUserProfile(message.from)
         .then((user) => {
-            message.reply(`Hey ${user.firstName}! I'm GPA Bot, I can help you calculate your Grade Point Average.`);
+            message.reply(`Hey ${user.firstName}! Enter a day to find the day's menu (i.e Tuesday)`);
         });
 });
+//Fires when a user sends a message
 bot.onTextMessage((message, bot) => {
-    if (message.body == 'help') {
-        message.reply('xyz');
+    if (message.body == 'Monday' || message.body == 'Tuesday' || message.body == 'Wednesday' || message.body == 'Thursday' || message.body == 'Friday') {
+        let menu = uwapi.foodservicesMenu().then(function(data) {
+          return data;
+        });
+        console.log(menu)
     } else {
       message.reply("Ahhhh I don't understand, type 'help' and I can give you a hand");
     }
 });
+////////////////////////BOT LOGIC////////////////////////////
 
-// Set up your server and start listening
+///////////////////////SERVER CONFIG/////////////////////////
 let server = http.createServer(bot.incoming());
 server.listen(process.env.PORT || config.port);
 console.log("Bot running on port " + config.port);
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port : "8000",
-  bot_name : "<your_bot_name>",
-  api_key : "<your_api_key",
-  base_url : "<your_base_url>"
+  bot_name : "gpa.bot",
+  api_key : "052020a2-1222-44d6-adf1-c7a0c1cef43e",
+  base_url : 'https://9ed46653.ngrok.io'
 };
