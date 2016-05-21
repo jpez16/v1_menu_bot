e6c2c3f7af41dcbcfcd88d7d4afb61983bd8ba86
Justin Pezzack <justin.pezzack@gmail.com>
2016-05-21 14:30:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port : "<your_port>",
-  bot_name : "<your_bot_name>",
-  api_key : "<your_api_key>",
-  uw_api_token: "<your_uw_api_key>",
-  base_url : 'https://xxxxxxxx.ngrok.io' //for testing purposes use ngrok
+  port         : "<your_port>",
+  bot_name     : "<your_bot_name>",
+  api_key      : "<your_api_key>",
+  uw_api_token : "<your_uw_api_key>",
+  base_url     : 'https://xxxx.ngrok.io' //for testing purposes use ngrok
 };
