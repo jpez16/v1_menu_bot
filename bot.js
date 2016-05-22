d383d0c9836374552df8817a410fae9363b34e1d
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-21 21:49:44 -0400

    bot much more robust

--- bot.js
+++ bot.js
@@ -4,17 +4,20 @@ let util = require('util');
 let http = require('http');
 let Bot  = require('@kikinteractive/kik');
 let config = require('./config');
-let apiToken = config.uw_api_token;
-let uwapi = require('uwapi')(apiToken);
+let uwapi = require('uwapi')(config.uw_api_token);
 /////////////////API ENDPOINT CONFIG////////////////////////
 let bot = new Bot({
     username: config.bot_name,
     apiKey: config.api_key,
     baseUrl: config.base_url
 });
+  uwapi.foodservicesMenu().then(function(data) {
+    console.log(data);
+  });
 bot.updateBotConfiguration();
 ///////////////////////////DEV///////////////////////////////
 bot.send('Bot Active', 'justinpezzack');
+console.log(config);
 ///////////////////////EVENT HANDLERS////////////////////////
 //Fires when a user talks to the bot for the very first time
 bot.onStartChattingMessage((message) => {
@@ -28,13 +31,24 @@ bot.onTextMessage((message, bot) => {
         let day_dict = {"Monday" : 0, "Tuesday" : 1, "Wednesday" : 2, "Thursday" : 3, "Friday" : 4}
         let msg = message['_state'].body;
         let day = day_dict[msg];
+        console.log(msg);
         if (msg == "Monday" || msg == "Tuesday" || msg == "Wednesday" || msg == "Thursday" || msg == "Friday") {
           uwapi.foodservicesMenu().then(function(data) {
-            //CHECK FOR 4 DAY WEEK  
+            //CHECK FOR 4 DAY WEEK
             let base_api_data = data['outlets'][0]['menu'];
-            if (base_api_date.length < 5){
-              //Have to deal with a shortened week.
-              console.log(base_api_data);
+            if(base_api_data.length < 5){
+              //4 Day week
+              if(base_api_data[0]['day'] == "Tuesday") {
+                day -=1;
+                if(msg == "Monday"){
+                  message.reply("There is no planned menu for " + msg + ".")
+                }
+              } else {
+                if(msg == "Friday"){
+                  message.reply("There is no planned menu for " + msg + ".")
+                }
+              }
+
 
             }
             //LUNCH
@@ -43,7 +57,7 @@ bot.onTextMessage((message, bot) => {
             message.reply("For Lunch on " + msg + ":");
             for (var i = 0; i < lunch.length; i ++) {
               lunch_string += (lunch[i]["product_name"]);
-              if (i < lunch.length-1){
+              if (i < lunch.length-1) {
                 lunch_string += ",\n";
               }
             }
