28bf9b9d29d344cc9a6a19cb973be832d99c39ad
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-22 22:03:40 -0400

    usability upgrades

--- bot.js
+++ bot.js
@@ -22,12 +22,11 @@ bot.updateBotConfiguration();
 console.log(config);
 ///////////////////////EVENT HANDLERS////////////////////////
 //Fires when a user talks to the bot for the very first time
+
 bot.onStartChattingMessage((message) => {
     bot.getUserProfile(message.from)
         .then((user) => {
-          bot.getUserProfile(message.from)
-          .then((user) => {
-              bot.send(Bot.Message.text("Hi I'm V1Menu Bot! I can show you whats being served in the cafe! Tap a day to see the menu.").addResponseKeyboard(
+              bot.send(Bot.Message.text("Hi I'm V1Menu Bot! I can show you whats being served in the café! Tap a day to see the menu.").addResponseKeyboard(
                 [
                   "Monday",
                   "Tuesday",
@@ -37,13 +36,35 @@ bot.onStartChattingMessage((message) => {
               ]), `${user.username}`);
         });
     });
-});
 //Fires when a user sends a message
 bot.onTextMessage((message) => {
-        let day_dict = {"Monday" : 0, "Tuesday" : 1, "Wednesday" : 2, "Thursday" : 3, "Friday" : 4}
-        let msg = message['_state'].body;
-        let day = day_dict[msg];
-        if (msg == "Monday" || msg == "Tuesday" || msg == "Wednesday" || msg == "Thursday" || msg == "Friday") {
+        let week_days =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+        let meals = ["lunch", "dinner"];
+        let lunch_asked = false;
+        let dinner_asked = false;
+        let day_dict = {"Monday" : 0, "Tuesday" : 1, "Wednesday" : 2, "Thursday" : 3, "Friday" : 4};
+        let msg = message['_state'].body.toLowerCase();
+        var query = false;
+        if(msg.indexOf('lunch')>-1) {
+          lunch_asked = true;
+        }
+        else if(msg.indexOf('dinner')>-1) {
+          dinner_asked = true;
+        }
+        else {
+          dinner_asked = true;
+          lunch_asked = true;
+        }
+        for (var i =0; i < 5; i++) {
+          if (msg.indexOf(week_days[i].toLowerCase()) > -1) {
+            query = true;
+            msg = week_days[i];
+            break;
+          }
+        }
+        //query == true when we have a valid date selected
+        if(query) {
+          let day = day_dict[msg];
           uwapi.foodservicesMenu().then(function(data) {
             //CHECK FOR 4 DAY WEEK
             let base_api_data = data['outlets'][0]['menu'];
@@ -61,29 +82,34 @@ bot.onTextMessage((message) => {
               }
             }
             //LUNCH
-            let lunch = base_api_data[day]['meals']['lunch'];
-            let lunch_string = "Lunch, 11:30 am - 2:00 pm:\n";
-            for (var i = 0; i < lunch.length; i ++) {
-              lunch_string += (lunch[i]["product_name"]);
-              if (i < lunch.length-1) {
-                lunch_string += ",\n";
+            if(lunch_asked){
+              let lunch = base_api_data[day]['meals']['lunch'];
+              let lunch_string = "Lunch, 11:30 am - 2:00 pm:\n";
+              for (var i = 0; i < lunch.length; i ++) {
+                lunch_string += (lunch[i]["product_name"]);
+                if (i < lunch.length-1) {
+                  lunch_string += ",\n";
+                }
               }
+              message.reply(lunch_string);
             }
-            message.reply(lunch_string);
             //DINNER
-            let dinner = base_api_data[day]['meals']['dinner'];
-            let dinner_string ="Dinner, 4:30 - 8:00 pm:\n";
-            for (var i = 0; i < dinner.length; i ++) {
-              dinner_string += (dinner[i]["product_name"]);
-              if (i < dinner.length-1){
-                dinner_string += ",\n";
+            if(dinner_asked) {
+              let dinner = base_api_data[day]['meals']['dinner'];
+              let dinner_string ="Dinner, 4:30 - 8:00 pm:\n";
+              for (var i = 0; i < dinner.length; i ++) {
+                dinner_string += (dinner[i]["product_name"]);
+                if (i < dinner.length-1){
+                  dinner_string += ",\n";
+                }
               }
+              message.reply(dinner_string);
             }
-            message.reply(dinner_string);
           });
         } else {
           message.reply("Invalid day.");
         }
+        //hacky but basically forces suggested responses to popup.
         bot.getUserProfile(message.from)
         .then((user) => {
             bot.send(Bot.Message.text('Tap a day to see a menu:').addResponseKeyboard(
@@ -96,6 +122,7 @@ bot.onTextMessage((message) => {
             ]), `${user.username}`);
         });
     });
+
 ///////////////////////SERVER CONFIG/////////////////////////
 let server = http.createServer(bot.incoming());
 server.listen(process.env.PORT || config.port);
