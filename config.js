e687968c4951a383e366d52450b02fe14a46cd89
justin-pezzack <justin.pezzack@uwaterloo.ca>
2016-05-26 23:14:01 -0400

    removed config

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port : "8000",
-  bot_name : "v1test",
-  api_key : "5619b2e3-c1e7-4447-b981-6006a8bead3e",
-  uw_api_token: "f3bf44dd39b2b55a98f0ea49390b157c",
-  base_url : "https://44305987.ngrok.io",
-};
